37fa247ef6f7f004db881a6615528ab3b0bdb139
rkokhatskyi <32820910+rkokhatskyi@users.noreply.github.com>
2024-04-21 13:53:39 +0400

    Make token address configurable

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-// config.js
+require('dotenv').config();
+
 module.exports = {
     rpcUrl: process.env.RPC_URL || 'wss://ethereum-rpc.publicnode.com',
-    tokenAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7'
+    tokenAddress: process.env.TOKEN_ADDRESS || '0xdac17f958d2ee523a2206206994597c13d831ec7'
 };
